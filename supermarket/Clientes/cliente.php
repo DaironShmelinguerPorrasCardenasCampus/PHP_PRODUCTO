97fcf3cbb2c405b09fdc90a2bdeab73d10bd7f91
DaironShmelinguerPorrasCardenasCampus <porras.n123456789@gmail.com>
2023-06-01 14:41:16 -0500

    feat: :white_check_mark: LINKEO PARA ACCEDER A PROVEEDOR

--- supermarket/Clientes/cliente.php
+++ supermarket/Clientes/cliente.php
@@ -56,7 +56,7 @@ $allCliente = $data-> obtenerCliente();
             <i class="bi bi-people"></i>
             <h3 style="margin: 0px;font-weight: 800;">Empleados</h3>
           </a>
-          <a href="../proveedores/proveedores.php" style="display: flex;gap:1px;">
+          <a href="../Proveedores/proveedor.php" style="display: flex;gap:1px;">
             <i class="bi bi-people"></i>
             <h3 style="margin: 0px;font-weight: 800;">Proveedores</h3>
           </a>
--- supermarket/categorias/categoria.php
+++ supermarket/categorias/categoria.php
@@ -54,7 +54,7 @@ $allCategory = $data->obtenerCategory();
             <i class="bi bi-people"></i>
             <h3 style="margin: 0px;font-weight: 800;">Empleados</h3>
           </a>
-          <a href="../proveedores/proveedores.php" style="display: flex;gap:1px;">
+          <a href="../Proveedores/proveedor.php" style="display: flex;gap:1px;">
             <i class="bi bi-people"></i>
             <h3 style="margin: 0px;font-weight: 800;">Proveedores</h3>
           </a>
